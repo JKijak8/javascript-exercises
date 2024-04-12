6a9d70c267eea4332a8dd7f60ca3f70814fa215a
JKijak8 <123284755+JKijak8@users.noreply.github.com>
2024-04-12 19:02:22 +0200

    Finish exercise 05_sumAll.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let higher, lower;
 
+    if(typeof num1 != "number" || typeof num2 != "number" || num1 < 0 || num2 < 0) {
+        return "ERROR"
+    }
+    
+    if(num1 > num2) {
+        higher = num1;
+        lower = num2;
+    }
+    else if(num1 < num2) {
+        higher = num2;
+        lower = num1
+    }
+    else {
+        return num1
+    }
+
+    let sum = lower;
+    for(let i = lower + 1; i <= higher; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line

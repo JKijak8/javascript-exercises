c22e98ad201324984bbf0eee2929d8c18b5ee9d0
JKijak8 <123284755+JKijak8@users.noreply.github.com>
2024-04-12 18:41:43 +0200

    Finish exercise 04_removeFromArray.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array,...toDelete) {
+    for(let i = 0; i < toDelete.length; i++) {
+        while(true) {
+            let index = array.indexOf(toDelete[i])
+            if (index < 0) {
+                break;
+            }
+            array.splice(index, 1);
+        }
+    } 
+    return array;
 };
 
 // Do not edit below this line

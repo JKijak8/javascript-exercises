a304a3801d8814e13f84c3df0c5b7e760092cada
JKijak8 <123284755+JKijak8@users.noreply.github.com>
2024-05-06 01:30:01 +0200

    chore(exercises): Finish exercise 08_calculator.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (numbers) {
+  return numbers != false
+    ? numbers.reduce((total, number) => total + number)
+    : 0;
 };
 
-const multiply = function() {
-
+const multiply = function (numbers) {
+  return numbers.reduce((total, number) => total * number);
 };
 
-const power = function() {
-	
+const power = function (number, power) {
+  const iterations = power - 1;
+  let total = number;
+  for (let i = 0; i < iterations; i++) {
+    total *= number;
+  }
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function (number) {
+  let factorial = 1;
+
+  for (let i = 1; i <= number; i++) {
+    factorial *= i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
@@ -29,5 +41,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };

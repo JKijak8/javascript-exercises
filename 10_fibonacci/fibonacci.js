e54904ab219bddf563d3e14c5ce09297f3a89a7e
JKijak8 <123284755+JKijak8@users.noreply.github.com>
2024-05-06 15:15:55 +0200

    chore(exercises): Finish exercise 10_fibonacci.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function (number) {
+  number = Number(number);
 
+  if (number < 0) {
+    return "OOPS";
+  }
+
+  let fibonacci = [0, 1];
+
+  while (fibonacci[number] === undefined) {
+    let length = fibonacci.length - 1;
+    fibonacci.push(fibonacci[length] + fibonacci[length - 1]);
+  }
+  return fibonacci[number];
 };
 
 // Do not edit below this line

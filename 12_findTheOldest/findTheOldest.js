1f84dfb10bb6985327451acd047ab71110ac7040
JKijak8 <123284755+JKijak8@users.noreply.github.com>
2024-05-06 16:03:29 +0200

    chore(exercises): Finish exercise 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  people.sort((firstPerson, secondPerson) => {
+    const firstAge = deadOrAlive(firstPerson) - firstPerson.yearOfBirth;
+    const secondAge = deadOrAlive(secondPerson) - secondPerson.yearOfBirth;
 
+    if (firstAge <= secondAge) {
+      return 1;
+    } else {
+      return -1;
+    }
+  });
+  return people[0];
 };
+
+function deadOrAlive(person) {
+  const currentYear = new Date().getFullYear();
+
+  if (person.yearOfDeath === undefined) {
+    return currentYear;
+  } else {
+    return person.yearOfDeath;
+  }
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;

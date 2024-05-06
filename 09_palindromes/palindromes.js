712da7ff21e1a9758f000cdf02849e35a2738885
JKijak8 <123284755+JKijak8@users.noreply.github.com>
2024-05-06 02:40:27 +0200

    chore(exercises): Finish exercise 09_palindromes.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (phrase) {
+  let split = phrase
+    .toLowerCase()
+    .split("")
+    .filter(
+      (item) => (item.toUpperCase() !== item || !isNaN(item)) && item !== " ",
+    );
 
+  const plainText = split.join("");
+  const reversed = split.reverse().join("");
+
+  return plainText === reversed;
 };
 
 // Do not edit below this line

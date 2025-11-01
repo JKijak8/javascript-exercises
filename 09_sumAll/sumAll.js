9b9f333fa2d389d8b087813a740752c51c6541cf
JKijak8 <123284755+JKijak8@users.noreply.github.com>
2025-11-02 00:40:37 +0100

    finish 09_sumAll.

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+  if (end < start) {
+    let buffer = start;
+    start = end;
+    end = buffer;
+  }
 
+  if (
+    start < 0 ||
+    end < 0 ||
+    !Number.isInteger(start) ||
+    !Number.isInteger(end) ||
+    Number.isNaN(start) ||
+    Number.isNaN(end)
+  ) {
+    return "ERROR";
+  }
+
+  let sum = 0;
+  for (let i = start; i <= end; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line

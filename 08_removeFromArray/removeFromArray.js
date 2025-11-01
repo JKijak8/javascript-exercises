7be5dcc5e6e39cd26b3a4639c82a16bdecbd678a
JKijak8 <123284755+JKijak8@users.noreply.github.com>
2025-11-02 00:27:13 +0100

    finish 08_removeFromArray.

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...toRemove) {
+  toRemove.forEach((element) => {
+    while (true) {
+      let index = array.indexOf(element);
+      if (index < 0) {
+        return;
+      }
+      array.splice(index, 1);
+    }
+  });
+  return array;
 };
 
 // Do not edit below this line

954583334eb1f63068973cccd49b67c0e0cb0988
JKijak8 <123284755+JKijak8@users.noreply.github.com>
2025-11-22 00:52:25 +0100

    feat: finish 14_fibonacci.

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  num = parseInt(num);
+  let last = 0;
+  let current = 1;
 
+  if (num === 0) {
+    return 0;
+  } else if (num < 0) return "OOPS";
+
+  for (let i = 1; i < num; i++) {
+    let buffer = current;
+    current += last;
+    last = buffer;
+  }
+  return current;
 };
 
 // Do not edit below this line

0291c5ca265ddfb694e408ed4958873b7187da32
JKijak8 <123284755+JKijak8@users.noreply.github.com>
2025-11-22 00:34:56 +0100

    feat: finish 13_palindromes.

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+/**
+ *
+ * @param {String} string
+ */
+const alphanumerical = "abcdefghijklmnopqrstuvwxyz1234567890";
 
+const palindromes = function (string) {
+  let cleaned = string
+    .toLowerCase()
+    .split("")
+    .filter((char) => alphanumerical.includes(char))
+    .join("");
+
+  let reversed = cleaned.split("").reverse().join("");
+
+  return cleaned === reversed;
 };
 
 // Do not edit below this line

94d278744e7b63f690789564b90e5ea830dbc1c0
JKijak8 <123284755+JKijak8@users.noreply.github.com>
2025-11-22 01:15:28 +0100

    feat: finish 16_findTheOldest.

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  return people.reduce((currentEldest, person) => {
+    let year = new Date().getFullYear();
 
+    console.log(currentEldest);
+    console.log(person);
+
+    if (!("yearOfDeath" in currentEldest)) {
+      currentEldest.yearOfDeath = year;
+    }
+
+    if (!("yearOfDeath" in person)) {
+      person.yearOfDeath = year;
+    }
+
+    let currentEldestAge =
+      currentEldest.yearOfDeath - currentEldest.yearOfBirth;
+    let personAge = person.yearOfDeath - person.yearOfBirth;
+
+    if (personAge > currentEldestAge) {
+      return person;
+    } else return currentEldest;
+  }, people[0]);
 };
 
 // Do not edit below this line
